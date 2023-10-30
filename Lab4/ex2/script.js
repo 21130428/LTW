6aeb98832b0c0f47a3b3428c6a74b5d84514f843
21130428 <21130428@st.hcmuaf.edu.vn>
2023-10-30 13:20:33 +0700

    update code

--- Lab4/ex2/script.js
+++ Lab4/ex2/script.js
@@ -1,11 +1,11 @@
 
 function myFuntion(){
-    var a = -3
+    var a = 4
     var b = -2
     var c = -1
     var delta = b*b - 4*a*c
-    var x1 = (-b+Math.sqrt(delta))/2*a
-    var x2 = (b+Math.sqrt(delta))/2*a
+    var x1 = (-b+Math.sqrt(delta))/(2*a)
+    var x2 = (-b-Math.sqrt(delta))/(2*a)
     var x3 = -b/2*a
     if(delta>0) document.write("Phương trình đã cho có 2 nghiệm là: "+x1+" và "+x2)
     if(delta<0) document.write("Phương trình đã cho có 1 nghiệm kép là: "+x3)
